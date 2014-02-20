bbbabde505b480c88a040ba4ebc3491152114120
Edward Tishkin <e.tishkin@m-st.ru>
2014-02-20 21:48:32 +0400

    Transliteration and Soundex mocks

---
+++ FuzzyString.Core/Utils/Soundex.cs
@@ -0,0 +1,10 @@
+﻿namespace FuzzySearch.Core.Utils
+{
+    public static class Soundex
+    {
+        public static string Convert(string arg)
+        {
+            return arg;
+        }
+    }
+}
