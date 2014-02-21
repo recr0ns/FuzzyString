83ea9a69fa457e5112c2e67fb6faf3cfc28f2fe3
Edward Tishkin <e.tishkin@m-st.ru>
2014-02-22 01:04:24 +0400

    Added phonetic algorithms project. Added Soundex. Added tests

---
+++ FuzzySearch.PhoneticAlgorithms.Test/SoundexTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FuzzySearch.PhoneticAlgorithms.Test
+{
+    [TestFixture]
+    public class SoundexTests
+    {
+        [TestCase("Azuron", "A265")]
+        [TestCase("Ackermann", "A265")]
+        [TestCase("Nagimov", "N251")]
+        [TestCase("Nesmeev","N251")]
+        [TestCase("As", "A200")]
+        public void SimpleSoundexTest(string input, string code)
+        {
+            Assert.That(Soundex.Convert(input, SoundexType.Simple), Is.EqualTo(code));
+        }
+
+        [TestCase("Nasimov","N382")]
+        [TestCase("Nazimov","N582")]
+        [TestCase("Nisenbaum", "N3818")]
+        [TestCase("As", "A3")]
+        
+        public void ImprovedSoundexTest(string input, string code)
+        {
+            Assert.That(Soundex.Convert(input, SoundexType.Improved), Is.EqualTo(code));
+        }
+    }
+}
---
+++ FuzzySearch.PhoneticAlgorithms/Soundex.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace FuzzySearch.PhoneticAlgorithms
+{
+    public class Soundex
+    {
+        private static readonly char[] IgnoredSymbols = { 'a', 'o', 'u', 'e', 'i', 'y', 'h', 'w' };
+        public static string Convert(string input)
+        {
+            return Convert(input, SoundexType.Simple);
+        }
+
+        public static string Convert(string input, SoundexType type)
+        {
+            ISoundexRule rule;
+            switch (type)
+            {
+                case SoundexType.Simple:
+                    rule = SimpleSoundex.Create();
+                    break;
+                case SoundexType.Improved:
+                    rule = ImprovedSoundex.Create();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("type");
+            }
+            return Convert(input, rule);
+        }
+
+        public static string Convert(string input, ISoundexRule rule)
+        {
+            if (string.IsNullOrWhiteSpace(input) || input.Length <= 1) return input;
+            var sb = new StringBuilder();
+            sb.Append(input[0]);
+            input = input.ToLower();
+            var prevCh = input[0];
+            var lastCode = '\0';
+            for (int index = 1, l = 1; index < input.Length; ++index)
+            {
+                var currCh = input[index];
+                if (!IgnoredSymbols.Contains(currCh) && currCh != prevCh)
+                {
+                    var code = rule.GetTable()[currCh] as char?;
+
+                    if (code.HasValue && code != lastCode)
+                    {
+                        sb.Append(code.Value);
+                        lastCode = code.Value;
+                        l++;
+                        if (rule.GetFixedLength() != 0 && l >= rule.GetFixedLength()) break;
+                    }
+                    else if (!code.HasValue)
+                    {
+                        sb.Append(currCh);
+                    }
+
+                    prevCh = currCh;
+                }
+            }
+            if (rule.GetFixedLength() != 0 && sb.Length <= rule.GetFixedLength())
+            {
+                for (var i = sb.Length + 1; i <= rule.GetFixedLength(); ++i) sb.Append("0");
+            }
+            return sb.ToString();
+        }
+    }
+
+    public enum SoundexType
+    {
+        Simple,
+        Improved
+    }
+
+    public interface ISoundexRule
+    {
+        Hashtable GetTable();
+        int GetFixedLength();
+    }
+
+    internal class SimpleSoundex : ISoundexRule
+    {
+        private Hashtable _table;
+
+        public SimpleSoundex()
+        {
+            _table = new Hashtable()
+            {
+                {'b', '1'},
+                {'p', '1'},
+                {'f', '1'},
+                {'v', '1'},
+                {'c', '2'},
+                {'s', '2'},
+                {'k', '2'},
+                {'j', '2'},
+                {'g', '2'},
+                {'q', '2'},
+                {'x', '2'},
+                {'z', '2'},
+                {'d', '3'},
+                {'t', '3'},
+                {'l', '4'},
+                {'m', '5'},
+                {'n', '5'},
+                {'r', '6'}
+            };
+        }
+
+        public static ISoundexRule Create()
+        {
+            return new SimpleSoundex();
+        }
+
+        public Hashtable GetTable()
+        {
+            return _table;
+        }
+
+        public int GetFixedLength()
+        {
+            return 4;
+        }
+    }
+
+    internal class ImprovedSoundex : ISoundexRule
+    {
+        private Hashtable _table;
+
+        public ImprovedSoundex()
+        {
+            _table = new Hashtable()
+            {
+                {'b', '1'},
+                {'p', '1'},
+                {'f', '2'},
+                {'v', '2'},
+                {'c', '3'},
+                {'s', '3'},
+                {'k', '3'},
+                {'j', '4'},
+                {'g', '4'},
+                {'q', '5'},
+                {'x', '5'},
+                {'z', '5'},
+                {'d', '6'},
+                {'t', '6'},
+                {'l', '7'},
+                {'m', '8'},
+                {'n', '8'},
+                {'r', '9'}
+            };
+        }
+
+        public static ISoundexRule Create()
+        {
+            return new ImprovedSoundex();
+        }
+
+        public Hashtable GetTable()
+        {
+            return _table;
+        }
+
+        public int GetFixedLength()
+        {
+            return 0;
+        }
+    }
+}
